a590f8a28b8d364b30bf5c7d5bb956a9eb6eadfd
Alvaro De La Torre <alvarodltp@gmail.com>
2018-01-25 12:13:48 -0600

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -59,3 +59,11 @@ Sodales libero suspendisse eu praesent arcu suspendisse faucibus erat dis urna v
 end
 
 puts "9 portfolio items created"
+
+3.times do |technology|
+      Portfolio.last.technologies.create!(
+      name: "Technology #{technology}",
+  )
+end
+
+puts "3 technologies created"
