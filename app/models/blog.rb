ff562410c31ac945f16a8f297dbc3339dd8338cf
Alvaro De La Torre <alvarodltp@gmail.com>
2018-01-26 13:28:33 -0600

    Customized registration, login, and logout routes

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,12 +1,10 @@
 class Blog < ApplicationRecord
+
   enum status: { draft: 0, published: 1}
   extend FriendlyId
   friendly_id :title, use: :slugged
 
   validates_presence_of :title, :body
-<<<<<<< HEAD
-=======
 
   belongs_to :topic
->>>>>>> 788ca7725f7edd877af0e68bedf011763fe4585d
 end
