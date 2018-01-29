17220ec44b68dd6b342c30ebd35dc7b4d4dd1012
Alvaro De La Torre <alvarodltp@gmail.com>
2018-01-29 11:48:17 -0600

    Added session tracker for dynamic source links

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,7 +1,7 @@
 class Portfolio < ApplicationRecord
-<<<<<<< HEAD
+
   validates_presence_of :title, :body, :main_image, :thumb_image
-=======
+
   has_many :technologies
   accepts_nested_attributes_for :technologies,
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
@@ -21,6 +21,5 @@ class Portfolio < ApplicationRecord
     self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
     self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
->>>>>>> 788ca7725f7edd877af0e68bedf011763fe4585d
 end
 
