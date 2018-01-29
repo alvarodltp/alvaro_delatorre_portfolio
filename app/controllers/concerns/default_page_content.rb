e622eff701ffc612d43f9b58ee6e9232d9f04594
Alvaro De La Torre <alvarodltp@gmail.com>
2018-01-29 13:58:28 -0600

    Added nav bar and added default header content

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,13 @@
+module DefaultPageContent
+  extend ActiveSupport::Concern
+  included do
+    before_action :page_defaults
+  end
+
+  def page_defaults
+    @page_title = "Alvaro De La Torre | My Portfolio Website"
+    @seo_keywords = "Alvaro De La Torre Portfolio"
+  end
+end
+
+
